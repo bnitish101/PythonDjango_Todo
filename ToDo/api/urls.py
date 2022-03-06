d148c2c90c5f559f32e627bc4835473077f92a9c
Nitish <bnitish101@gmail.com>
2022-03-06 21:39:59 +0530

    install django restframework, create migration. add superuser, create api urls

--- ToDo/api/urls.py
+++ ToDo/api/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.apiOverView)
+    path('', views.apiOverView, name='api-overview'),
+    path('todo-list/', views.toDoList, name='todo-list'),
+    path('todo-detail/<str:pk>/', views.toDoDetail, name='todo-detail'),
 ]
--- ToDo/api/views.py
+++ ToDo/api/views.py
@@ -1,6 +1,33 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+# from django.shortcuts import render
+# from django.http import JsonResponse
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from api.models import ToDo
+from api.serializers import ToDoSerializer
+
 # Create your views here.
 
+@api_view(['GET'])
 def apiOverView(request):
-    return JsonResponse('Json response', safe=False)
+    api_urls = {
+        'ToDo List' : 'todo-list/',
+        'Todo Detail' : '/todo-detail/<str:pk>/',
+        'Create Todo' : '/create-todo/',
+        'Update Todo' : '/update-todo/<str:pk>/',
+        'Delete Todo' : '/delete-todo/<str:pk>/',
+    }
+    return Response(api_urls)
+
+@api_view(['GET'])
+def toDoList(request):
+    todo = ToDo.objects.all()
+    serializer = ToDoSerializer(todo, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def toDoDetail(request, pk):
+    todo_detail = ToDo.objects.get(pk=pk)
+    serializer = ToDoSerializer(todo_detail, many=False)
+    return Response(serializer.data)
